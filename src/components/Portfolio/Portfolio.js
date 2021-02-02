f87fabdcbdd5688b40950a8b9464664b46dbd915
nico-fri <nico.fri@outlook.de>
2021-02-03 00:50:39 +0100

    removed useRef

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import './Portfolio.css'
 import data from '../../data/data'
 import Information from '../Information/Information'
